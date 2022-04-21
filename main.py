34fe1714d75dcb457d1f7de31a15fd1fb6a9e3b0
Athirson <Athirsonarceus@gmail.com>
2022-04-21 14:26:10 -0300

    Project showcase image

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ window.title("Kanye Says...")
 window.config(padx=50, pady=50)
 
 canvas = Canvas(width=300, height=414)
-background_img = PhotoImage(file="projects/kanye-quotes/background.png")
+background_img = PhotoImage(file="/home/athirson/Programming/bootcamp/projects/kanye-quotes-app/background.png")
 canvas.create_image(150, 207, image=background_img)
 quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=(
     "Arial", 30, "bold"), fill="white")
 canvas.grid(row=0, column=0)
 
-kanye_img = PhotoImage(file="projects/kanye-quotes/kanye.png")
+kanye_img = PhotoImage(file="/home/athirson/Programming/bootcamp/projects/kanye-quotes-app/kanye.png")
 kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
 kanye_button.grid(row=1, column=0)
 
